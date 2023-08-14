63bcabe14275de704509d8f2cb6e969d412be443
ranjithsf4081 <127499234+SivaranjithN@users.noreply.github.com>
2023-08-14 12:09:53 +0530

    Added new sample for KB documentation in SfBadgeView control.

---
+++ BadgeView/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BadgeView;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+}
+public class Model
+{
+    public string Category { get; set; }
+    public int Count { get; set; }
+    public Color Color { get; set; }
+}
+public class ViewModel
+{
+    public ObservableCollection<ModelList> Items { get; set; }
+
+    public ViewModel()
+    {
+        var modellList1 = new ModelList()
+            {
+                new Model(){Category = "Primary", Count = 27, Color = Color.FromArgb("#e3165b")},
+                new Model(){Category = "Social", Count = 12, Color = Color.FromArgb("#3f51b5")},
+                new Model(){Category = "Promotions", Count = 7, Color = Color.FromArgb("#4d841d")},
+                new Model(){Category = "Updates", Count = 1, Color = Color.FromArgb("#2088da")},
+            };
+        modellList1.Heading = "Inbox";
+
+        var modellList2 = new ModelList()
+            {
+                new Model(){Category = "Starred", Count = 0, Color = Colors.Brown},
+                new Model(){Category = "Important", Count = 12, Color = Colors.Brown},
+                new Model(){Category = "Sent", Count = 0, Color = Color.FromArgb("#e3165b")},
+                new Model(){Category = "Drafts", Count = 0, Color = Color.FromArgb("#e3165b")},
+            };
+        modellList2.Heading = "All Labels";
+
+        Items = new ObservableCollection<ModelList>()
+            {
+                modellList1,
+                modellList2
+            };
+    }
+}
+
+public class ModelList : ObservableCollection<Model>
+{
+    public ObservableCollection<Model> Models => this;
+    public string Heading { get; set; }
+}
---
+++ BadgeView/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BadgeView;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
